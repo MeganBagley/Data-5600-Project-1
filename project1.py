de43ea10f1ba1031a25224119d65af9d6170dffe
Megan Bagley <a02418137@aggies.usu.edu>
2026-09-19 17:01:38 -0600

    Revise sim_data function for realistic data simulation
    Updated simulation function to generate random data for snow making, days open, and runs using normal distribution.

--- project1.py
+++ project1.py
@@ -11,9 +11,9 @@ rng = np.random.default_rng(42)
 # Specify a function to simulate data
 # predictors: snow making (in), # of days open last year, # of runs at the resort, avg snow fall(in)
 def sim_data(n, beta_0, beta_snow_making, beta_days_open_last, beta_runs, beta_avgsnow, beta_sigma):
-    snow_making = (6, 1, size=n)  # 
-    days_open_last = (1, 0.3, size=n)  # 
-    runs = 
+    snow_making = rng.normal(300, 200, size=n)  # 
+    days_open_last = rng.normal(130, 20, size=n)  # 
+    runs = rng.normal(
     avgsnow = rng.normal(5, 1, size=n)  # 
     error = rng.normal(0, sigma, size=n)
     sentiment = beta_0 + beta_snow_making * snow_making + beta_days_open_last * days_open_last + beta_runs * runs + beta_avgsnow * avgsnow + error
