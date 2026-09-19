b4e72e9390f083eee6e9cab9a2c0f733849f30ac
Megan Bagley <a02418137@aggies.usu.edu>
2026-09-19 17:04:10 -0600

    Fix simulation parameters in sim_data function

--- project1.py
+++ project1.py
@@ -13,13 +13,13 @@ rng = np.random.default_rng(42)
 def sim_data(n, beta_0, beta_snow_making, beta_days_open_last, beta_runs, beta_avgsnow, beta_sigma):
     snow_making = rng.normal(300, 200, size=n)  # 
     days_open_last = rng.normal(130, 20, size=n)  # 
-    runs = rng.normal(
-    avgsnow = rng.normal(5, 1, size=n)  # 
+    runs = rng.normal(70,20,size=n)
+    avgsnow = rng.normal(400, 150, size=n)  # 
     error = rng.normal(0, sigma, size=n)
     sentiment = beta_0 + beta_snow_making * snow_making + beta_days_open_last * days_open_last + beta_runs * runs + beta_avgsnow * avgsnow + error
     return snow_making, days_open_last, runs, avgsnow, competitor_pricing, error
 
 sentiment, snow_making, days_open_last, runs, avgsnow, error = sim_data(
-    n=200, beta_0=500, snow_making=0.3, days_open_last=6,
-    runs=-0.3, avgsnow=5, sigma=1
+    n=200, beta_0=500, snow_making=290, days_open_last=140,
+    runs=72, avgsnow=420, sigma=1
 )
