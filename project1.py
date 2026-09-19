8b552b7d0610726e932b23422a37361b83a2ab1b
Megan Bagley <a02418137@aggies.usu.edu>
2026-09-19 17:07:03 -0600

    Enhance comments in sim_data functiona
    Updated comments in sim_data function for clarity.

--- project1.py
+++ project1.py
@@ -12,11 +12,11 @@ rng = np.random.default_rng(42)
 # predictors: snow making (in), # of days open last year, # of runs at the resort, avg snow fall(in)
 def sim_data(n, beta_0, beta_snow_making, beta_days_open_last, beta_runs, beta_avgsnow, beta_sigma):
     
-    snow_making = rng.normal(300, 200, size=n)  #
-    days_open_last = rng.normal(130, 20, size=n)  # 
-    runs = rng.normal(70,20,size=n)
-    avgsnow = rng.normal(400, 150, size=n)  # 
-    error = rng.normal(0, sigma, size=n)
+    snow_making = rng.normal(300, 200, size=n)  # based on average snow making in inches in US resorts
+    days_open_last = rng.normal(130, 20, size=n)  # based on average days open in ski resorts in 2018 season
+    runs = rng.normal(70,20,size=n) # based on the average run count in the ski resorts
+    avgsnow = rng.normal(400, 150, size=n)  #  based on the average snowfall
+    error = rng.normal(0, sigma, size=n) 
     sentiment = beta_0 + beta_snow_making * snow_making + beta_days_open_last * days_open_last + beta_runs * runs + beta_avgsnow * avgsnow + error
     return snow_making, days_open_last, runs, avgsnow, error
 
