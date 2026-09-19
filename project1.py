b7bb4c72a19c7c92d7d8ce60e93bb5194236c27b
Megan Bagley <a02418137@aggies.usu.edu>
2026-09-19 17:26:49 -0600

    Fix return values and update sim_data function call
    Refactor sim_data function to return values correctly and update function call.

--- project1.py
+++ project1.py
@@ -17,7 +17,6 @@ def sim_data(n, beta_0, beta_snow_making, beta_days_open_last, beta_runs, beta_a
     runs = rng.normal(70, 20, size=n).clip(min=1)
     avgsnow = rng.normal(400, 150, size=n).clip(min=0)
     error = rng.normal(0, sigma, size=n)
-    return sentiment, snow_making, days_open_last, runs, avgsnow, error
 
     sentiment_latent = (
         beta_0
@@ -28,10 +27,11 @@ def sim_data(n, beta_0, beta_snow_making, beta_days_open_last, beta_runs, beta_a
         + error
     )
     sentiment = np.clip(np.round(sentiment_latent), 1, 7)  # 7-point scale
-)
-sentiment, snow_making, runs, avgsnow, error = sim_data(
-    n=200, beta_0=3, snow_making=.5, runs=3,
-    avgsnow=3, sigma=1
+    return sentiment, snow_making, days_open_last, runs, avgsnow, error
+
+sentiment, snow_making, days_open_last, runs, avgsnow, error = sim_data(
+    n=200, beta_0=-1.0, beta_snow_making=0.002, beta_days_open_last=0.02,
+    beta_runs=0.015, beta_avgsnow=0.002, sigma=0.8
 )
 
 # create dataframe for storage
